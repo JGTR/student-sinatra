1bddb96147c0e09f175e396ad0f07770f97cbaaf
Jordan Trevino <jordan.trevino@gmail.com>
2013-06-26 16:23:28 -0400

    finished creating new student and edit

--- app.rb
+++ app.rb
@@ -17,19 +17,17 @@ module StudentSite
       erb :'students/index', :layout => true
     end
 
+    get '/students/new' do  
+      erb :'newstudentform', :layout => true
+    end
+
     get '/students/:id' do
       student_id = params[:id]
       @student = Student.find(student_id)
       erb :'students/student', :layout => false
     end
 
-    get '/newstudentform' do
-      
-      erb :'newstudentform', :layout => false
-
-    end
-
-    post '/filledstudentform' do
+    post '/students/' do
       @name    = params[:name]
       @tagline = params[:tagline]
       @bio     = params[:bio]
@@ -42,22 +40,23 @@ module StudentSite
 
       student.save
        
-      erb :'filledstudentform', :layout => false
-
+      erb :'filledstudentform', :layout => true
     end
 
     get '/students/:id/edit' do
       student_id = params[:id]
       @student = Student.find(student_id)
+      erb :'editstudentform', :layout => false
+    end
+
+    post '/students/:id' do
+      @student = Student.find(params[:id])
+      @student.name = params[:name]
+      @student.bio = params[:bio]
+      @student.tagline = params[:tagline]
+      @student.save
       erb :'students/student', :layout => false
     end
-
-    get '/editstudentform' do
-      
-      erb :'editstudentform', :layout => false
-
-    end
-
 
 #we want a web form for new students
 #creates a new student
